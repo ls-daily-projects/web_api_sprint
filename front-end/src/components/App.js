63c19adb33b3a98e3c4fbddcd603b4df40c3600c
Mikis Woodwinter <mikis.woodwinter@gmail.com>
2019-07-12 10:46:43 -0700

    Add ability to view all projects in CRA
    Signed-off-by: Mikis Woodwinter <mikis.woodwinter@gmail.com>

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -1,9 +1,42 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import axios from "axios"
+
+import LoadingIndicator from "./LoadingIndicator"
 
 const App = () => {
+    const [isLoading, setIsLoading] = useState(true)
+    const [projects, setProjects] = useState([])
+
+    useEffect(() => {
+        axios
+            .get("/api/projects")
+            .then(({ data }) => {
+                setProjects(data)
+            })
+            .catch(err => console.log(err))
+            .finally(() => setIsLoading(false))
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem("projects", JSON.stringify(projects))
+    }, [projects])
+
+    if (isLoading) {
+        return <LoadingIndicator />
+    }
     return (
         <>
             <h1>Projects</h1>
+            <ul>
+                {projects.map(({ id, name, description, completed }) => (
+                    <li key={id}>
+                        {completed ? "✅" : "❌"}
+                        <h3>{name}</h3>
+
+                        <small>{description}</small>
+                    </li>
+                ))}
+            </ul>
         </>
     )
 }
